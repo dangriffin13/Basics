64cace3ddb463b473804f4ca2fe5f96ceca986da
dangriffin13 <dangriffin13@gmail.com>
2018-05-15 00:14:24 -0400

    argv

---
+++ argv_practice.py
@@ -0,0 +1,19 @@
+
+import sys
+
+def print_script_name():
+    print("The name of the script is {}".format(sys.argv[0]))
+
+
+def print_args():
+    i = 1
+    while i <= len(sys.argv)-1:
+        print(sys.argv[i])
+        i += 1
+
+
+if __name__ == "__main__":
+    print_script_name()
+    if len(sys.argv) > 1:
+        print_args()
+
