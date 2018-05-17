ec1c5604fff33ee5190c052696ee7c6c9dafb1cc
dangriffin13 <dangriffin13@gmail.com>
2018-05-16 21:06:03 -0400

    sorts and searches

---
+++ sorts_and_searches.py
@@ -0,0 +1,31 @@
+
+
+def linear_search(arr):
+    
+
+def binary_search(arr):
+
+
+def bubble_sort(arr):
+
+
+def insertion_sort(arr):
+
+
+def merge_sort(arr):
+
+
+def quick_sort(arr):
+
+
+def tim_sort(arr):
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Sort and Search algos are available")
