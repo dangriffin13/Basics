5b89cd8d4bc22711faeee17faa94b5ab2f1a753a
dangriffin13 <dangriffin13@gmail.com>
2018-05-23 20:06:08 -0400

    unittest

---
+++ unittest.py
@@ -0,0 +1,35 @@
+
+
+import unittest
+
+
+def add_2(number):
+    return number + 2
+
+def concatenate(string1, string2):
+    return string1 + string2
+
+def is_odd(number):
+    if number % 2 == 1:
+        return True
+    else:
+        return False
+    
+class PracticeTestCase(unittest.TestCase):
+    
+    def test_5_odd(self):
+        self.assertTrue(is_odd(5))
+        
+    def test_14_even(self):
+        self.assertFalse(is_odd(14))
+        
+    def test_concatenate(self):
+        self.assertTrue(concatenate("test ","successful")=="test successful")
+        
+    def test_negative_one_add_2(self):
+        self.assetTrue(add_2(-1)==1)
+
+
+
+if __name__ == "__main__":
+    print('Unit tests are running')
