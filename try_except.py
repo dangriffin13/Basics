662f402e36b9847f76554a9a79653d3f97fc5e08
dangriffin13 <dangriffin13@gmail.com>
2018-05-28 23:55:18 -0400

    zerodiv and key

--- try_except.py
+++ try_except.py
@@ -1,12 +1,24 @@
 
-def zero_division(number):
+def zero_division(dividend, divisor):
+    try:
+        result = dividend/divisor
+    except ZeroDivisionError as e:
+        print("You're attempting to divide by zero")
 
 
 def end_of_file(file):
 
 
 
-def key_error(key, data):
+def key_error(key, data=None):
+    if data is None:
+        data = {'test':'passed', 1:'chocolate', 3:'vanilla'}
+    try:
+        val = data[key]
+    except KeyError as e:
+        print("That key is not in your data")
+
+
 
 
 def type_error_date(string):
@@ -14,7 +26,7 @@ def type_error_date(string):
 
 
 def type_error_int(number):
-    
+
 
 
 
