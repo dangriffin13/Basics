80d2d383b651da5ce81fe39f5155a74fa6b456c9
dangriffin13 <dangriffin13@gmail.com>
2018-05-28 23:38:47 -0400

    try wishlist

---
+++ try_except.py
@@ -0,0 +1,22 @@
+
+def zero_division(number):
+
+
+def end_of_file(file):
+
+
+
+def key_error(key, data):
+
+
+def type_error_date(string):
+
+
+
+def type_error_int(number):
+    
+
+
+
+if __name__ == "__main__":
+    print("Try...Except is available")
