4d777f8bb01c3735f0635d8df4bf97371dc03197
dangriffin13 <dangriffin13@gmail.com>
2018-06-01 00:17:18 -0400

    prints

--- zip_enumerate.py
+++ zip_enumerate.py
@@ -1,10 +1,16 @@
 
 def enumerate_inputs(data):
-    enumerated = enumerate(date)
+    enumerated = enumerate(data)
+    print('Printing enumerated items...')
+    for item in enumerated:
+        print(item)
 
 
 def zip_two_inputs(iterable1, iterable2):
     zipped = zip(iterable1, iterable2)
+    print('Printing zipped items...')
+    for item in zipped:
+        print(item)
 
 
 
@@ -14,5 +20,5 @@ if __name__ == "__main__":
     arr1 = ['a','b','c','d','e']
     arr2 = ['alpha','beta', 'gamma', 'delta', 'epsilon']
 
-    print('list 1: ', arr1)
-    print('list 2: ', arr2)
+    print('list 1 (variable name arr1): ', arr1)
+    print('list 2 (variable name arr2): ', arr2)
