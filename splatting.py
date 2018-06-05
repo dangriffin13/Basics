5f2d9f975813c90415143d0ac7b9757470cc81a1
dangriffin13 <dangriffin13@gmail.com>
2018-06-04 23:49:12 -0400

    keyword splatting

--- splatting.py
+++ splatting.py
@@ -1,10 +1,14 @@
 
+tri_args = ['first','second','third']
+tri_kwargs = {'1':'first','2':'second','3':'third'}
 
 def receive_three_splatted_args(arg1, arg2, arg3):
-    pass
+    print('arg1: ', arg1)
+    print('arg3: ', arg3)
 
 def receive_three_splatted_kwargs(kwarg1, kwarg2, kwarg3):
-    pass
+    print('kwarg1: ', kwarg1)
+    print('kwarg3: ', kwarg3)
 
 def receive_unsplatted_args(*args):
     for arg in args:
@@ -20,3 +24,9 @@ def receive_unsplatted_kwargs(**kwargs):
 
 if __name__ == "__main__":
     print("Splat operations up and running")
+    print('tri_args: ', tri_args)
+    print('splat tri_args: ')
+    receive_three_splatted_args(*tri_args)
+    print('tri_kwargs: ', tri_kwargs)
+    print('splat tri kwargs: ')
+    receive_three_splatted_kwargs(**tri_kwargs)
