b625a0a97c92029b242c8c994bd6d314bdddfc1d
dangriffin13 <dangriffin13@gmail.com>
2018-06-05 22:16:56 -0400

    keyword splat

--- splatting.py
+++ splatting.py
@@ -1,22 +1,22 @@
 
 tri_args = ['first','second','third']
-tri_kwargs = {'1':'first','2':'second','3':'third'}
+tri_kwargs = {'one':'first','two':'second','three':'third'}
 
 def receive_three_splatted_args(arg1, arg2, arg3):
     print('arg1: ', arg1)
     print('arg3: ', arg3)
 
-def receive_three_splatted_kwargs(kwarg1, kwarg2, kwarg3):
-    print('kwarg1: ', kwarg1)
-    print('kwarg3: ', kwarg3)
+def receive_three_splatted_kwargs(one,two,three):
+    print('kwarg1: ', 'one')
+    print('kwarg3: ', 'three')
 
 def receive_unsplatted_args(*args):
     for arg in args:
         print(arg)
 
-def receive_unsplatted_kwargs(**kwargs):
-    for key, val in kwargs:
-        print(key, value)
+def receive_splatted_kwargs(**kwargs):
+    for key, val in kwargs.items():
+        print(key, val)
 
 
 
@@ -28,5 +28,9 @@ if __name__ == "__main__":
     print('splat tri_args: ')
     receive_three_splatted_args(*tri_args)
     print('tri_kwargs: ', tri_kwargs)
-    print('splat tri kwargs: ')
+    print('send splatted tri kwargs: ')
     receive_three_splatted_kwargs(**tri_kwargs)
+    print('send tri_kwargs dict and splat in function: ')
+    receive_splatted_kwargs(**tri_kwargs)
+
+
