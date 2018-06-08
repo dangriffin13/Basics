9b84e7752ab1ee630d7a8a166588f856cbf294c9
dangriffin13 <dangriffin13@gmail.com>
2018-06-08 01:29:25 -0400

    deque

--- collections.py
+++ collections.py
@@ -22,6 +22,11 @@ lincolns_words = collections.Counter(words)
 top_10 = lincolns_words.most_common(10)
 
 
+dq = collections.deque(list('sesquipedalian'))
+
+
+
+
 if __name__ == "__main__":
     print("Collections file is running")
 
@@ -33,3 +38,13 @@ if __name__ == "__main__":
     print(results_dict)
     print("Ten most used words in Gettysburg Address: ")
     print(top_10)
+    print("Deque functions:")
+    print(dq)
+    print("Pop: ", dq.pop())
+    print("Pop left: ", dq.popleft())
+    print(dq)
+    print("Extend left: 'new'", dq.extendleft(list('new')))
+    print("Extend right: 'new'", dq.extend(list('new')))
+    print(dq)
+    print("Reverse: ", dq.reverse())
+    print(dq)
