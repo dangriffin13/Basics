7d793b7b4d859252d5ba31dd6dd550eb55776dc6
dangriffin13 <dangriffin13@gmail.com>
2018-06-09 18:02:07 -0400

    hashlib sha256

---
+++ hashing.py
@@ -0,0 +1,14 @@
+
+import hashlib
+
+
+def sha_256(string):
+    stored_hash = hashlib.sha256(string):
+    print(stored_hash)
+
+
+
+
+
+if __name__ == "__main__":
+    print('You can begin hashing')
