14975cd76d643795b76e1300e332186dbfd2dfac
dangriffin13 <dangriffin13@gmail.com>
2018-06-13 00:08:10 -0400

    gen primes for euler 10

--- generators.py
+++ generators.py
@@ -7,6 +7,15 @@ def simple_generator(n):
 
 #jeff knupp example
 
+def select_primes(values):
+    return [n for n in values if is_prime(n)]
+
+def gen_primes(n):
+    while True: #the while loop continuously increments n and yields if prime
+        if is_prime(n):
+            yield n
+        n += 1
+
 def is_prime(n):
     if n > 1:
         if n ==2:
@@ -19,6 +28,16 @@ def is_prime(n):
         return True
     else:
         return False
+
+
+def euler_10(n): #sum all primes >= n, <= 2,000,000
+    total = 2
+    for next_prime in gen_primes(n):
+        if next_prime < 2000000:
+            total += next_prime
+        else:
+            return total
+
 
 if __name__ == "__main__":
     print('Generators are ready to yield')
