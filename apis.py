f39e6e0075d2ea43e5eeff57113f2811e6e05bfc
dangriffin13 <dangriffin13@gmail.com>
2018-06-15 10:40:57 -0400

    yahoo weather is functional

--- apis.py
+++ apis.py
@@ -1,38 +1,47 @@
 
-import urllib, urllib.request, json
+import urllib, urllib.request
+import json
+import requests
 
 
 #Yahoo
 
 #client ID
-consumer_key
+consumer_key = 'dj0yJmk9V05KSmFNTGhXb0REJmQ9WVdrOVRrcHBRVmQwTkdrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD03ZA--'
 
 #client secret
-consumer_secret
+#consumer_secret
 
-baseurl = "https://query.yahooapis.com/v1/public/yql?"
+baseurl = "https://query.yahooapis.com/v1/public/yql?q"
 
 manhattan_woeid = 12761355
 nyc_woeid = 2459115
 
 
-
-def get_nyc_weather():
-    https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+def get_nyc_weather(baseurl, manhattan_woeid):
+    query = f'select item.condition from weather.forecast where woeid={manhattan_woeid}&format=json'
+    url = f'{baseurl}={query}'
+    #return url
+    response = requests.get(url)
+    response = response.json()
+    conditions = response['query']['results']['channel']['item']['condition']
+    return conditions
 
 def get_nyc_forecast(baseurl, manhattan_woeid):
     query = f'select * from weather.forecast where woeid={manhattan_woeid}&format=json'
-
     url = f'{baseurl}={query}'
+    #return url
+    response = requests.get(url)
+    return response.json()
 
 
-    req = requests.get(url)
 
-
-#"https://query.yahooapis.com/v1/public/yql?=select * from weather.forecast where woeid=2459115&format=json"
+#Quandl
 
 
 quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
+
+
 
 """
 Call URL: 
@@ -44,12 +53,15 @@ https://www.quandl.com/api/v3/datasets/CME
     /CLH2018.json?start_date=2018-01-01&api_key=pWjXmxamqHYAMueDfPUE
 """
 
-def quandl_api_wrapper(database_code, dataset_code): #let's pull actual json off the web to practice
-    
+
+def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018
     quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?'
+
     response = requests.get(quote_url + quandl_api_key)
     return response.json()
 
 
 if __name__ == "__main__":
     print('APIs are ready')
+    conditions = get_nyc_weather(baseurl, manhattan_woeid)
+    print(conditions['text'], conditions['temp'])
