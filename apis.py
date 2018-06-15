7547c6e10472befecaa179cefc1580b44a9def7f
dangriffin13 <dangriffin13@gmail.com>
2018-06-14 22:48:39 -0400

    quandl api

--- apis.py
+++ apis.py
@@ -29,7 +29,27 @@ def get_nyc_forecast(baseurl, manhattan_woeid):
     req = requests.get(url)
 
 
-"https://query.yahooapis.com/v1/public/yql?=select * from weather.forecast where woeid=2459115&format=json"
+#"https://query.yahooapis.com/v1/public/yql?=select * from weather.forecast where woeid=2459115&format=json"
+
+
+quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
+
+"""
+Call URL: 
+https://www.quandl.com/api/v3/datasets/DATABASE_CODE
+    /DATASET_CODE.csv?api_key=YOUR_API_KEY_HERE
+
+Example:
+https://www.quandl.com/api/v3/datasets/CME
+    /CLH2018.json?start_date=2018-01-01&api_key=pWjXmxamqHYAMueDfPUE
+"""
+
+def quandl_api_wrapper(database_code, dataset_code): #let's pull actual json off the web to practice
+    
+    quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?'
+    response = requests.get(quote_url + quandl_api_key)
+    return response.json()
+
 
 if __name__ == "__main__":
     print('APIs are ready')
