255ed1c856039b7b1e5cf9979c4c459e860a8d4f
dangriffin13 <dangriffin13@gmail.com>
2018-06-17 23:07:48 -0400

    basic class with some methods

--- classes.py
+++ classes.py
@@ -1,6 +1,22 @@
 
 
-def Account(self, number, user):
-    def __init__(self, number, user):
-        self.number = account_number
-        self.user = account_owner
+def Account(self, number, user, balance=0):
+    def __init__(self, number, user, balance):
+        self.account_number = number
+        self.owner = user
+        self.balance = balance
+
+    def deposit(self, transaction):
+        self.balance += transaction
+        return transaction, self.balance
+
+    def withdrawal(self, transaction):
+        if transaction > self.balance:
+            raise RunTimeError('Insufficient funds.')
+        self.balance -= transaction
+        return transaction, self.balance
+
+    def view_balance(self):
+        return self.balance
+
+    
