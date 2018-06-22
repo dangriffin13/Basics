a61785e2e7d0646d4889ed649fa948420e744af8
dangriffin13 <dangriffin13@gmail.com>
2018-06-22 08:05:45 -0400

    email refactor

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,14 +1,14 @@
 
 
 def regex_basic_email_validator(string):
-    if re.match(r"[A-Za-z0-9_-]+@[A-Za-z0-9]+\.[a-zA-Z]{1,3}$", string):
+    if re.match(r"^([A-Za-z0-9_\.-]+)@([A-Za-z0-9_\.-]+)\.([a-zA-Z]{2,6})$", string):
         return True
     else:
         return False
 
 
-def regex_password_validator(string):
-    if re.match(r"[A-Za-z0-9]{8,}", string):
+def regex_password_validator(string): #does not include special chars
+    if re.match(r"^[A-Za-z0-9]{8,}$", string):
         return True
     else:
         return False
