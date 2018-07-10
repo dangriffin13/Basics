11cbcb119a9f033c8bbb9e8e91907d71d46f0f55
dangriffin13 <dangriffin13@gmail.com>
2018-07-10 00:23:39 -0400

    series

---
+++ pandas_methods.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import pandas as pd
+
+index = ['a','b','c','d']
+vals = ['100','3.14','2.71','32']
+
+ser = pd.Series(vals, index)
+
+
+if __name__ == "__main__":
+    print("Kung fu pandas")
